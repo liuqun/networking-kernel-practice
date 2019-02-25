4a0703faabd16e4f5feb8027e0c360cd8a2b4c8c
刘群 <517067180@qq.com>
2019-02-25 19:03:16 +0800

    Fix up tab indent and a gcc warning
    keep local variable declaration position strict at the beginning
    of a function.
    
    Signed-off-by: 刘群 <517067180@qq.com>

--- netlink_test/nlexample.c
+++ netlink_test/nlexample.c
@@ -16,35 +16,32 @@ static int myvar;
 static void
 nl_callback(struct sk_buff *skb)
 {
-   pr_err("the current value is %d\n", myvar);
+	pr_info("the current value is %d\n", myvar);
 }
 
 static int __init nlexample_init(void)
 {
-
-    pr_err("init nlexample module\n");
 	struct netlink_kernel_cfg cfg = {
 		.groups = NLEX_GRP_MAX,
 		.input	= nl_callback,
 	};
 
-    pr_err("init nlexample module before create\n");
+	pr_debug("init nlexample module\n");
+
+	pr_debug("init nlexample module before create\n");
 	nlsk = netlink_kernel_create(&init_net, NETLINK_EXAMPLE, &cfg);
 	if (nlsk == NULL) {
 		pr_err("Can't create netlink example\n");
 		return -ENOMEM;
 	}
-    else
-    {
-        pr_err("nlsk is %d\n", nlsk);
-    }
+	pr_err("nlsk ptr is %p\n", nlsk);
 	return 0;
 }
 
-void __exit nlexample_exit(void)
+static void __exit nlexample_exit(void)
 {
 	netlink_kernel_release(nlsk);
-    pr_err("eixt nlexample module\n");
+	pr_debug("exit nlexample module\n");
 }
 
 module_init(nlexample_init);
